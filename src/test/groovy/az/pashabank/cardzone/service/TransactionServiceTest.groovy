1fe72523ca89b7e6886fe68f2fb26d68766bddfc
smehraliyev404 <saidmehraliyev@gmail.com>
2024-06-28 17:57:54 +0400

    one test case added for transaction service

--- src/test/groovy/az/pashabank/cardzone/service/TransactionServiceTest.groovy
+++ src/test/groovy/az/pashabank/cardzone/service/TransactionServiceTest.groovy
@@ -3,14 +3,44 @@ package az.pashabank.cardzone.service
 import az.pashabank.cardzone.dao.repository.CardRepository
 import az.pashabank.cardzone.dao.repository.TransactionRepository
 import az.pashabank.cardzone.mapper.TransactionMapper
+import az.pashabank.cardzone.model.dto.TransactionDto
+import az.pashabank.cardzone.model.enumfiles.TransactionType
+import az.pashabank.cardzone.model.exception.NoCardFoundByIdException
 import spock.lang.Specification
+import spock.lang.Subject
 
 class TransactionServiceTest extends Specification {
+
+    @Subject
+    private TransactionService transactionService
+    private TransactionRepository transactionRepository
+    private TransactionMapper transactionMapper
+    private CardRepository cardRepository
+
     void setup() {
-        def transactionRepository = Mock(TransactionRepository)
-        def transactionMapper = Mock(TransactionMapper)
-        def cardRepository = Mock(CardRepository)
-        def transactionService = new TransactionService(transactionMapper, transactionRepository, cardRepository)
+        transactionRepository = Mock()
+        transactionMapper = Mock()
+        cardRepository = Mock()
+        transactionService = new TransactionService(transactionMapper, transactionRepository, cardRepository)
+    }
+
+    def "create: should throw an exception when no card is found by requested id"() {
+        given:
+        def cardId = 404L
+        def transactionDto = new TransactionDto(
+                TransactionType.DEBIT,
+                BigDecimal.valueOf(404),
+                true
+        )
+
+        cardRepository.findById(cardId) >> Optional.empty()
+
+        when:
+        transactionService.create(transactionDto, cardId)
+
+        then:
+        NoCardFoundByIdException exception = thrown()
+        exception.getMessage() == "Transaction cannot be made, as the card with id:" + cardId + " does not exist."
     }
 
     def "create: should create a new transaction"() {
